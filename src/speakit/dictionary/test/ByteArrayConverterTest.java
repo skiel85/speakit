c1d417d14b5d3605268d0a7fb8c1962b127d1acb
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-03-21 22:50:00 +0000

    Se cambio el nombre de la clase ByteArrayConverter por Converter

--- src/speakit/dictionary/test/ByteArrayConverterTest.java
+++ src/speakit/dictionary/test/ByteArrayConverterTest.java
@@ -3,34 +3,34 @@ package speakit.dictionary.test;
 import org.junit.Assert;
 import org.junit.Test;
 
-import speakit.dictionary.ByteArrayConverter;
+import speakit.dictionary.Converter;
 
 
 public class ByteArrayConverterTest {
 	@Test
 	public void testConvertToInt() throws Exception {
 		byte[] array=new byte[]{0x01,0x0A,0x10,0x0F};
-		Assert.assertEquals(17436687, new ByteArrayConverter().toInt(array));
+		Assert.assertEquals(17436687, new Converter().toInt(array));
 	}
 	
 	@Test
 	public void testConvertToInt2() throws Exception {
 		byte[] array=new byte[]{0x11, 0x11, 0x23, 0x25};
-		Assert.assertEquals(286335781, new ByteArrayConverter().toInt(array ));
+		Assert.assertEquals(286335781, new Converter().toInt(array ));
 	}
 	
 	@Test
 	public void testConvertToLong() throws Exception {
 		byte[] array=new byte[]{0x11, 0x11, 0x23, 0x25,0x00, 0x00, 0x00, 0x00};
 		long num = 1229802815069618176L;
-		Assert.assertEquals( num, new ByteArrayConverter().toLong(array));
+		Assert.assertEquals( num, new Converter().toLong(array));
 	}
 	
 	@Test
 	public void testConvertToDouble() throws Exception {
 		Double d = 1231.11;
-		byte[] array=new ByteArrayConverter().toByta(d); 
-		Assert.assertEquals( d, new ByteArrayConverter().toDouble(array));
+		byte[] array=new Converter().toByta(d); 
+		Assert.assertEquals( d, new Converter().toDouble(array));
 	}
 	
 	

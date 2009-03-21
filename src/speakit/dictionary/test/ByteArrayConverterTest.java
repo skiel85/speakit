4a2220d978e0841540171293949cdea1aa2858c3
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-03-21 22:43:11 +0000

    Se agregó llenó la clase ByteArrayConverter con metodos encontrados en internet. Se probaron algunos. Referencia:http://www.daniweb.com/code/snippet644.html

--- src/speakit/dictionary/test/ByteArrayConverterTest.java
+++ src/speakit/dictionary/test/ByteArrayConverterTest.java
@@ -12,4 +12,26 @@ public class ByteArrayConverterTest {
 		byte[] array=new byte[]{0x01,0x0A,0x10,0x0F};
 		Assert.assertEquals(17436687, new ByteArrayConverter().toInt(array));
 	}
+	
+	@Test
+	public void testConvertToInt2() throws Exception {
+		byte[] array=new byte[]{0x11, 0x11, 0x23, 0x25};
+		Assert.assertEquals(286335781, new ByteArrayConverter().toInt(array ));
+	}
+	
+	@Test
+	public void testConvertToLong() throws Exception {
+		byte[] array=new byte[]{0x11, 0x11, 0x23, 0x25,0x00, 0x00, 0x00, 0x00};
+		long num = 1229802815069618176L;
+		Assert.assertEquals( num, new ByteArrayConverter().toLong(array));
+	}
+	
+	@Test
+	public void testConvertToDouble() throws Exception {
+		Double d = 1231.11;
+		byte[] array=new ByteArrayConverter().toByta(d); 
+		Assert.assertEquals( d, new ByteArrayConverter().toDouble(array));
+	}
+	
+	
 }

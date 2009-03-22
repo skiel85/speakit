0258be05cefdd31a818fcdff579e4f923af35d28
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-03-22 18:29:36 +0000

    Agregada clase SerializableLong, para serializar longs.

---
+++ trunk/src/speakit/dictionary/SerializableLong.java
@@ -0,0 +1,37 @@
+package speakit.dictionary;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class SerializableLong extends SerializablePrimitiveType {
+
+	long value = 0;
+	
+	public SerializableLong(long value){
+		this.value = value;
+	}
+	
+	public SerializableLong(){
+		this.value = 0;
+	}
+	
+	public long getValue(){
+		return this.value;
+	}
+
+	@Override
+	public int getSerializationSize() {
+		return Long.SIZE/BYTE_SIZE;
+	}
+
+	@Override
+	public void actuallyDeserialize(InputStream in) throws IOException {
+		this.value = ByteArrayConverter.toLong(readBytes(in));
+	}
+
+	@Override
+	public void actuallySerialize(OutputStream out) throws IOException {
+		out.write(ByteArrayConverter.toByta(this.value));
+	} 
+}

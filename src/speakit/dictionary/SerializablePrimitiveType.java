0c31254edb753a35996c460fb0865accb04b7267
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-03-22 16:18:48 +0000

    Se comentó la clase SerializablePrimitiveType Se creó una prueba de serialización completa para SerializableInteger y se la hizo pasar corrigiendo un error.

--- src/speakit/dictionary/SerializablePrimitiveType.java
+++ src/speakit/dictionary/SerializablePrimitiveType.java
@@ -6,36 +6,76 @@ import java.io.OutputStream;
 
 public abstract class SerializablePrimitiveType {
 
+	/**
+	 * Tamaño de un byte expresado en bits
+	 */
+	protected static final int BYTE_SIZE = 8;
+	
 	public SerializablePrimitiveType() {
 		super();
 	}
 
-
+	/**
+	 * Lee una tantos bytes como la cantidad de bytes necesarios para serializarse 
+	 * @param in
+	 * @return
+	 * @throws IOException
+	 */
 	protected byte[] readBytes(InputStream in) throws IOException{
 		return readBytes(in,this.getSerializationSize());
 	}
-	
-	protected byte[] readBytes(InputStream in,int count) throws IOException{
-		byte[] data=new byte[count];
+
+	/**
+	 * Lee una cantidad específica de bytes de un inputStream
+	 * @param in
+	 * @param count
+	 * @return
+	 * @throws IOException
+	 */
+	protected byte[] readBytes(InputStream in,int lenght) throws IOException{
+		byte[] data=new byte[lenght];
 		in.read(data );
 		return data;
 	}
-	
 
+	/**
+	 * Delega al metodo actuallyDeserialize
+	 * @param in
+	 * @return cantidad de bytes leidos desde in
+	 * @throws IOException
+	 */
 	public long deserialize(InputStream in) throws IOException {
 		this.actuallyDeserialize(in);
-		return this.getSerializationSize();
+		return (long)this.getSerializationSize();
 	}
-	
+
+	/**
+	 * Delega al metodo actuallySerialize
+	 * @param out
+	 * @return cantidad de bytes escritos en out
+	 * @throws IOException
+	 */
 	public long serialize(OutputStream out) throws IOException {
 		this.actuallySerialize( out);
-		return this.getSerializationSize();
+		return (long)this.getSerializationSize();
 	}
-	
 
+	/**
+	 * @return la cantidad de bytes que ocupa la serializacion de su valor
+	 */
 	public abstract int getSerializationSize();
-	
+
+	/**
+	 * Hidrata el objeto 
+	 * @param out
+	 * @throws IOException
+	 */
 	public abstract void actuallyDeserialize(InputStream out) throws IOException ;
 
+	/**
+	 * Persiste el objeto como bytes
+	 * @param in
+	 * @throws IOException
+	 */
 	public abstract void actuallySerialize(OutputStream in) throws IOException ;
 }

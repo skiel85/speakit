cae8c3cb900571784a71355fe3a3507185fb1a11
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-03-22 19:38:30 +0000

    Corregida prueba del diccionario.

--- trunk/src/speakit/dictionary/test/AudioDictionaryImplTest.java
+++ trunk/src/speakit/dictionary/test/AudioDictionaryImplTest.java
@@ -1,27 +1,36 @@
 package speakit.dictionary.test;
 
+import java.io.File;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import speakit.dictionary.AudioDictionaryImpl;
+import speakit.dictionary.AudioFile;
+import speakit.dictionary.AudioIndexFile;
 
 public class AudioDictionaryImplTest {
 
 	private AudioDictionaryImpl sut;
+	private File file1;
+	private File file2;	
 	
 	@Before
 	public void setUp() throws Exception {
-		/*
-		File audioFile = new File("audioFile.dat");
-		File audioIndexFile = new File("audioIndexFile.dat");
-		this.sut = new AudioDictionaryImpl();
-		*/
+		file1 = File.createTempFile(this.getClass().getName(), ".dat");
+		file2 = File.createTempFile(this.getClass().getName(), ".dat");
+		
+		AudioFile audioFile = new AudioFile(file1);
+		AudioIndexFile audioIndexFile = new AudioIndexFile(file2);
+		this.sut = new AudioDictionaryImpl(audioIndexFile, audioFile);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		file1.delete();
+		file2.delete();
 	}
 	
 	@Test

e748e4f5b3d33fb63e862ad3aa277763b160583f
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-03-25 19:07:46 +0000

    Se modificó un caso de pruebas para que sea de "caja negra". Se elimino un caso de pruebas porque no tenia sentido probar la implementacion.

--- trunk/src/speakit/dictionary/test/AudioFileTest.java
+++ trunk/src/speakit/dictionary/test/AudioFileTest.java
@@ -13,7 +13,7 @@ import speakit.dictionary.files.audiofile.AudioFile;
 public class AudioFileTest {
 	File file;
 	AudioFile sut;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		this.file = File.createTempFile(this.getClass().getName(), ".dat");
@@ -27,36 +27,23 @@ public class AudioFileTest {
 
 	@Test
 	public void testAddAndGetAudio() throws IOException {
-		byte[] audio = new byte[] {10, -25, 32, 64, -122, 89};
+		byte[] audio = new byte[] { 10, -25, 32, 64, -122, 89 };
 		this.sut.addAudio(audio);
 		Assert.assertArrayEquals(audio, this.sut.getAudio(0));
 	}
-	
+
 	@Test
 	public void testAddSomeAudiosAndGetOne() throws IOException {
-		byte[] audio1 = new byte[] {10, -25, 32, 64, -122, 89, 55, 0, -3, 102};
-		byte[] audio2 = new byte[] {4, 82, 36, 25, -30, -120, 78};
-		byte[] audio3 = new byte[] {89, 23, 2, -1, 0, 64, 64, 9, -44};
-		this.sut.addAudio(audio1);
-		this.sut.addAudio(audio2);
-		this.sut.addAudio(audio3);
-		Assert.assertArrayEquals(audio1, this.sut.getAudio(0));
-		Assert.assertArrayEquals(audio2, this.sut.getAudio(4 + audio1.length));
-		Assert.assertArrayEquals(audio3, this.sut.getAudio(4 + audio1.length + 4 + audio2.length));
-		Assert.assertArrayEquals(audio2, this.sut.getAudio(4 + audio1.length));
-	}
-	
-	@Test
-	public void testAddAudioAndGetOffset() throws IOException {
-		byte[] audio1 = new byte[] {10, -25, 32, 64, -122, 89, 55, 0, -3, 102};
-		byte[] audio2 = new byte[] {4, 82, 36, 25, -30, -120, 78};
-		byte[] audio3 = new byte[] {89, 23, 2, -1, 0, 64, 64, 9, -44};
+		byte[] audio1 = new byte[] { 10, -25, 32, 64, -122, 89, 55, 0, -3, 102 };
+		byte[] audio2 = new byte[] { 4, 82, 36, 25, -30, -120, 78 };
+		byte[] audio3 = new byte[] { 89, 23, 2, -1, 0, 64, 64, 9, -44 };
 		long offset1 = this.sut.addAudio(audio1);
 		long offset2 = this.sut.addAudio(audio2);
 		long offset3 = this.sut.addAudio(audio3);
-		Assert.assertEquals(0, offset1);
-		Assert.assertEquals(4 + audio1.length, offset2);
-		Assert.assertEquals(4 + audio1.length + 4 + audio2.length, offset3);
+		Assert.assertArrayEquals(audio1, this.sut.getAudio(offset1));
+		Assert.assertArrayEquals(audio2, this.sut.getAudio(offset2));
+		Assert.assertArrayEquals(audio3, this.sut.getAudio(offset3));
+		Assert.assertArrayEquals(audio2, this.sut.getAudio(offset2));
 	}
 
 }

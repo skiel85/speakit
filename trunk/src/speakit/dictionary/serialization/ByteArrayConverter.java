368eeddea125161986f73a4fca4676e1455d9d3e
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-04-02 07:01:43 +0000

    Se comentaron funciones de ByteArrayConverter y se agregaron pruebas

--- trunk/src/speakit/dictionary/serialization/ByteArrayConverter.java
+++ trunk/src/speakit/dictionary/serialization/ByteArrayConverter.java
@@ -1,43 +1,86 @@
 package speakit.dictionary.serialization;
 
-public class ByteArrayConverter {
+public class ByteArrayConverter { 
 	/**
 	 * Convierte un array de bytes a un int. Cada byte del array formará parte
 	 * del int, el 1er byte será el que ocupe los primeros 8 bits del int, el
 	 * segundo ocupará los proximos 8 y así sucesivamente.
 	 * 
+	 * Codificado en big-endian, pues en el byte[0] está el byte mas significativo
 	 * @param b
 	 * @return
-	 */
-
+	 */ 
+	public static int toInt(byte[] b, int offset) { 
+		if (b == null || b.length < 4){ 
+			return 0;
+		}
+		//el & 0xff es para limpiar posible basura en memoria
+		return  (0xff & (b[0 + offset]))<< 24 | // XXXX XXXX|0000 0000|0000 0000|0000 0000
+		 		(0xff & (b[1 + offset]))<< 16 | // 0000 0000|XXXX XXXX|0000 0000|0000 0000				
+		 		(0xff & (b[2 + offset]))<< 8  | // 0000 0000|0000 0000|XXXX XXXX|0000 0000
+		 		(0xff & (b[3 + offset]))<< 0  ; // 0000 0000|0000 0000|0000 0000|XXXX XXXX
+		
+	}
+	 /**
+	  * Convierte un array de bytes en un int.
+	  * @param data
+	  * @return
+	  */
+	public static int toInt(byte[] data) {
+		return toInt(data,0);
+	}
+	
 	/**
-	 * Convierte un array de bytes a long. Lo hace en base a toInt.
+	 * Convierte un array de bytes a long. 
 	 * 
 	 * @param b
 	 * @return
 	 */
-
-	public static byte[] toByta(int data) {
-		return new byte[] { (byte) ((data >> 24) & 0xff), (byte) ((data >> 16) & 0xff), (byte) ((data >> 8) & 0xff), (byte) ((data >> 0) & 0xff), };
+	 public static long toLong(byte[] b) { 
+		 if (b == null || b.length < 8) {
+			 return 0L;
+		 } 
+		 
+		 return (long) (0xff & b[0]) << 56 | 
+				(long) (0xff & b[1]) << 48 | 
+				(long) (0xff & b[2]) << 40 | 
+				(long) (0xff & b[3]) << 32 |
+				(long) (0xff & b[4]) << 24 | 
+				(long) (0xff & b[5]) << 16 | 
+				(long) (0xff & b[6]) << 8  | 
+				(long) (0xff & b[7]) << 0  ; 
+	 }
+	 
+	
+	 /**
+	  * Convierte un int en un array de bytes
+	  * @param data
+	  * @return
+	  */
+	public static byte[] toByteArray(int data) {
+		return new byte[]{
+				(byte) ((data >> 24) & 0xff), 
+				(byte) ((data >> 16) & 0xff), 
+				(byte) ((data >> 8)  & 0xff), 
+				(byte) ((data >> 0)  & 0xff),};
 	}
 
-	public static byte[] toByta(long data) {
-		return new byte[] { (byte) ((data >> 56) & 0xff), (byte) ((data >> 48) & 0xff), (byte) ((data >> 40) & 0xff), (byte) ((data >> 32) & 0xff), (byte) ((data >> 24) & 0xff), (byte) ((data >> 16) & 0xff), (byte) ((data >> 8) & 0xff), (byte) ((data >> 0) & 0xff), };
-	}
-
-	public static int toInt(byte[] data) {
-		if (data == null || data.length != 4)
-			return 0x0;
-		// ----------
-		return (int) ((0xff & data[0]) << 24 | (0xff & data[1]) << 16 | (0xff & data[2]) << 8 | (0xff & data[3]) << 0);
-	}
-
-	public static long toLong(byte[] data) {
-		if (data == null || data.length != 8)
-			return 0x0;
-		// ----------
-		return (long) ((long) (0xff & data[0]) << 56 | (long) (0xff & data[1]) << 48 | (long) (0xff & data[2]) << 40 | (long) (0xff & data[3]) << 32 | (long) (0xff & data[4]) << 24 | (long) (0xff & data[5]) << 16 | (long) (0xff & data[6]) << 8 | (long) (0xff & data[7]) << 0);
-	}
+	 /**
+	  * Convierte un long en un array de bytes
+	  * @param data
+	  * @return
+	  */
+	public static byte[] toByteArray(long data) {
+		return new byte[]{
+				(byte) ((data >> 56) & 0xff), 
+				(byte) ((data >> 48) & 0xff), 
+				(byte) ((data >> 40) & 0xff), 
+				(byte) ((data >> 32) & 0xff), 
+				(byte) ((data >> 24) & 0xff),
+				(byte) ((data >> 16) & 0xff), 
+				(byte) ((data >> 8)  & 0xff), 
+				(byte) ((data >> 0)  & 0xff),};
+	} 
 
 	public static String toString(byte[] data) {
 		return (data == null) ? null : new String(data);

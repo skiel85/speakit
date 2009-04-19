c67fa3cdfd28fdd2ac3bf3cad0cf0fd70092af0d
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-04-19 06:02:25 +0000

    Hecho que SecuentialRecordFile implemente RecordFile.

--- trunk/src/speakit/dictionary/files/RecordFile.java
+++ trunk/src/speakit/dictionary/files/RecordFile.java
@@ -1,5 +1,7 @@
 package speakit.dictionary.files;
 
+import java.io.IOException;
+
 public interface RecordFile<RECTYPE extends Record> {
-	public void insertRecord(RECTYPE record);
+	public void insertRecord(RECTYPE record) throws IOException;
 }

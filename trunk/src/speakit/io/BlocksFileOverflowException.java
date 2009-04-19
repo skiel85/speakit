1521c7ab07283bbd1724843168ee2f42da263a17
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-04-19 15:46:28 +0000

    Se crearon excepciones para la clase BlocksFile

--- trunk/src/speakit/io/BlocksFileOverflowException.java
+++ trunk/src/speakit/io/BlocksFileOverflowException.java
@@ -1,10 +1,25 @@
 package speakit.io;
 
-public class BlocksFileOverflowException extends Exception {
+public class BlocksFileOverflowException extends BlocksFileException {
 
 	private static final long	serialVersionUID	= 1L;
+	private int					overflowLenght		= 0;
+	private final int			blockSize;
+	private final int			actualDataLength;
 
-	public BlocksFileOverflowException( int blockSize, int actualDataLength) {
-		super("Se esperaban como máximo: " + blockSize + " pero se recibieron: " + actualDataLength );
+	public BlocksFileOverflowException(int blockSize, int actualDataLength) {
+		super();
+		this.blockSize = blockSize;
+		this.actualDataLength = actualDataLength;
+		overflowLenght = actualDataLength - blockSize;
+	}
+
+	public int getOverflowLenght() {
+		return overflowLenght;
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage() + "Se esperaban como máximo: " + blockSize + " pero se recibieron: " + actualDataLength + ". " + this.overflowLenght + " bytes en overflow.";
 	}
 }

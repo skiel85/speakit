f7cbdfa0b0ec5a6d60f417cd9d5d2f5cb6c3099c
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-04-20 02:13:27 +0000

    Corregido iterador de bloques. Implementado método para obtener registro a partir de una clave.

--- trunk/src/speakit/dictionary/files/RecordFile.java
+++ trunk/src/speakit/dictionary/files/RecordFile.java
@@ -6,6 +6,6 @@ import speakit.dictionary.serialization.Field;
 
 public interface RecordFile<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> {
 	public void insertRecord(RECTYPE record) throws IOException, RecordSerializationException;
-	public RECTYPE getRecord(KEYTYPE key) throws IOException;
-	public boolean contains(KEYTYPE key) throws IOException;
+	public RECTYPE getRecord(KEYTYPE key) throws IOException, RecordSerializationException;
+	public boolean contains(KEYTYPE key) throws IOException, RecordSerializationException;
 }

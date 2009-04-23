aea3c810d467cb2d7a9d8e37da01f0c28d61c4a6
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-04-23 05:04:05 +0000

    Se subió el esqueleto de las clases necesarias para implementar FrontCoding. Se incluyen pruebas que fallan.

---
+++ src/speakit/compression/FrontCodedWord.java
@@ -0,0 +1,21 @@
+package speakit.compression;
+
+public class FrontCodedWord {
+	
+	private final short matchingCharacters;
+	private final String endingCharacters;
+
+	public FrontCodedWord(short matchingCharacters,String endingCharacters){
+		this.matchingCharacters = matchingCharacters;
+		this.endingCharacters = endingCharacters;				
+	}
+
+	public String getEndingCharacters() {
+		return endingCharacters;
+	}
+
+	public short getMatchingCharacters() {
+		return matchingCharacters;
+	}
+	
+}
---
+++ src/speakit/compression/FrontCodingWordEncoder.java
@@ -0,0 +1,10 @@
+package speakit.compression;
+
+
+public class FrontCodingWordEncoder {
+
+	public FrontCodedWord encode(String string) {
+		return new FrontCodedWord((short)0,string);		
+	}
+
+}

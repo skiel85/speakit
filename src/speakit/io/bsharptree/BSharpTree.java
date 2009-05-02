59be680bda6125102fa4875e56b224e4ac418781
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-02 18:40:25 +0000

    Comenzada a programar la lógica de overflow de la raiz.

--- src/speakit/io/bsharptree/BSharpTree.java
+++ src/speakit/io/bsharptree/BSharpTree.java
@@ -1,7 +1,10 @@
 package speakit.io.bsharptree;
 
+import java.io.File;
 import java.io.IOException;
 
+import speakit.io.blockfile.BasicBlockFile;
+import speakit.io.blockfile.BasicBlockFileImpl;
 import speakit.io.record.Field;
 import speakit.io.record.Record;
 import speakit.io.record.RecordSerializationException;
@@ -9,6 +12,21 @@ import speakit.io.recordfile.RecordFile;
 
 public class BSharpTree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implements RecordFile<RECTYPE, KEYTYPE> {
 	private BSharpTreeNode<RECTYPE, KEYTYPE> root;
+	private BasicBlockFile blockFile;
+
+	public BSharpTree(File file) {
+		this.blockFile = new BasicBlockFileImpl(file);
+	}
+
+	public void create(int nodeSize) throws IOException {
+		this.blockFile.create(nodeSize);
+		this.root = new BSharpTreeLeafNode<RECTYPE, KEYTYPE>(this.blockFile);
+	}
+
+	public void load() throws IOException {
+		this.blockFile.load();
+		// TODO Cargar la raiz
+	}
 
 	@Override
 	public boolean contains(KEYTYPE key) throws IOException, RecordSerializationException {
@@ -24,7 +42,22 @@ public class BSharpTree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> 
 	public void insertRecord(RECTYPE record) throws IOException, RecordSerializationException {
 		this.root.insertRecord(record);
 		if (this.root.isInOverflow()) {
+			if (this.root.getLevel() == 0) {
+//				BSharpTreeIndexNode<RECTYPE, KEYTYPE> newRoot = new BSharpTreeIndexNode<RECTYPE, KEYTYPE>(this.blockFile);
+//				BSharpTreeLeafNode<RECTYPE, KEYTYPE>[] leafs;
+//				leafs[0] = new BSharpTreeLeafNode<RECTYPE, KEYTYPE>(this.blockFile);
+//				leafs[1] = new BSharpTreeLeafNode<RECTYPE, KEYTYPE>(this.blockFile);
+//				leafs[2] = new BSharpTreeLeafNode<RECTYPE, KEYTYPE>(this.blockFile);
+//
+//				// leaf1.copyAllRecordsFrom(this.root);
+//				this.root.balance(leafs);
+//
+//				newRoot.indexChild(leafs[0]);
+//				newRoot.indexChild(leafs[1]);
+//				newRoot.indexChild(leafs[2]);
+//				this.root = newRoot;
 
+			}
 		}
 	}
 
--- src/speakit/io/bsharptree/BSharpTreeLeafNode.java
+++ src/speakit/io/bsharptree/BSharpTreeLeafNode.java
@@ -2,12 +2,21 @@ package speakit.io.bsharptree;
 
 import java.io.IOException;
 
+import speakit.io.blockfile.BasicBlockFile;
 import speakit.io.record.Field;
 import speakit.io.record.Record;
 import speakit.io.record.RecordSerializationException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class BSharpTreeLeafNode<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> extends BSharpTreeNode<RECTYPE, KEYTYPE> {
 
+	private BasicBlockFile blockFile;
+	private BSharpTreeIndexNodeRecord record;
+	
+	public BSharpTreeLeafNode(BasicBlockFile blockFile) {
+		this.blockFile = blockFile;
+	}
+	
 	@Override
 	public boolean contains(KEYTYPE key) throws IOException, RecordSerializationException {
 		// TODO Auto-generated method stub
@@ -23,12 +32,20 @@ public class BSharpTreeLeafNode<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends
 	@Override
 	public void insertRecord(RECTYPE record) throws IOException, RecordSerializationException {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
-	public boolean isInOverflow() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isInOverflow() throws RecordSerializationException, IOException {
+		return (this.record.serialize().length > this.blockFile.getBlockSize() * 2);
+	}
+	
+	@Override
+	public int getLevel() {
+		return 0;
+	}
+
+	@Override
+	public void balance(BSharpTreeNode<RECTYPE, KEYTYPE>[] nodes) {
+		throw new NotImplementedException();
 	}
 }

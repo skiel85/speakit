2b7018f875566e0d6a98812aee7d016737d3d03d
seba_monoloco@hotmail.com <seba_monoloco@hotmail.com@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-04 06:02:53 +0000

    terminado front coding

--- trunk/src/speakit/compression/FrontCodingCompressor.java
+++ trunk/src/speakit/compression/FrontCodingCompressor.java
@@ -1,15 +1,59 @@
 package speakit.compression;
 
-public class FrontCodingCompressor {
 
+
+/**
+ * Clase para poder codificar y decodificar palabras.
+ * 
+ */
+public class FrontCodingCompressor {
+	
+	/**
+	 * Metodo que permite codificar un array de palabras
+	 * 
+	 * @param strings
+	 *            
+	 */
 	public FrontCodedWord[] compress(String[] strings) {
-		// TODO Auto-generated method stub
-		return null;
+		FrontCodedWord[] compressedWords = new FrontCodedWord[strings.length];
+		FrontCodingWordEncoder encoder = new FrontCodingWordEncoder();
+		for(int i=0;i<strings.length;i++){
+			compressedWords[i] = encoder.encode(strings, strings[i]);
+		}
+		
+		return compressedWords;
 	}
 
+	
+	/**
+	 * Metodo que permite decodificar un array de palabras comprimidas
+	 * 
+	 * @param frontCodedWords
+	 */
 	public String[] decompress(FrontCodedWord[] frontCodedWords) {
-		// TODO Auto-generated method stub
-		return null;
+	    String[] decompressedWords = new String[frontCodedWords.length];
+	    FrontCodingWordDecoder decoder = new FrontCodingWordDecoder(); 
+	    int position = 0;
+	    while(position < frontCodedWords.length){
+	    	if(position == 0){
+	    		String word = decoder.decode(frontCodedWords[position]);
+	    		decompressedWords[position] = word;
+	    	}else{
+	    	    String beforeWord = decompressedWords[position--];
+	    		FrontCodedWord actualWord = frontCodedWords[position];
+	    		short matchingCharacters = actualWord.getMatchingCharacters(); 
+	    		char[] array = new char[beforeWord.length()];
+	    		array = beforeWord.toCharArray();
+	    		String decompressedWord = "";
+	    		for(int i=0; i<matchingCharacters;i++){
+	    			decompressedWord = decompressedWord + array[i];
+	    		}
+	    		decompressedWord = decompressedWord + actualWord.getEndingCharacters();
+	    		decompressedWords[position] = decompressedWord;
+	    	} 
+	    }
+		
+		return decompressedWords;
 	}
 
 }

046cb791e99b264f81e754df726ab04e8133bbc3
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-09 13:16:18 +0000

    B#Tree: Implementado el split en los nodos indices. Pruebas ok. Falta split en la raiz.

--- src/speakit/io/bsharptree/Tree.java
+++ src/speakit/io/bsharptree/Tree.java
@@ -13,6 +13,7 @@ import speakit.io.record.Field;
 import speakit.io.record.Record;
 import speakit.io.record.RecordFactory;
 import speakit.io.record.RecordSerializationException;
+import speakit.io.record.StringField;
 import speakit.io.recordfile.RecordFile;
 
 @SuppressWarnings("unchecked")
@@ -20,14 +21,14 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 	/**
 	 * Indica la cantidad de bloques que ocupa la raiz
 	 */
-	protected static final int ROOT_NODE_BLOCKS_QTY = 2;
-	protected static final int NON_ROOT_NODE_BLOCKS_QTY = 1;
+	protected static final int	ROOT_NODE_BLOCKS_QTY		= 2;
+	protected static final int	NON_ROOT_NODE_BLOCKS_QTY	= 1;
 
-	protected TreeNode root;
-	protected BasicBlockFile blockFile;
-	protected RecordEncoder encoder;
+	protected TreeNode			root;
+	protected BasicBlockFile	blockFile;
+	protected RecordEncoder		encoder;
 
-	private RecordFactory recordFactory;
+	private RecordFactory		recordFactory;
 
 	public Tree(File file, RecordFactory recordFactory) {
 		this(file, recordFactory, new DefaultRecordEncoder(recordFactory));
@@ -106,9 +107,9 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 		} else {
 			TreeNode node;
 			if (parent.getLevel() == 1) {
-				node = new TreeLeafNode(this, nodeNumber , 1);
+				node = new TreeLeafNode(this, nodeNumber, 1);
 			} else if (parent.getLevel() > 1) {
-				node = new TreeIndexNode(this,nodeNumber, 1);
+				node = new TreeIndexNode(this, nodeNumber, 1);
 			} else {
 				throw new IllegalArgumentException("No se puede construir un nodo con padre con level: " + parent.getLevel());
 			}
@@ -133,15 +134,34 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 		return ROOT_NODE_BLOCKS_QTY;
 	}
 
+	boolean	found	= false;
 	@Override
 	public long insertRecord(RECTYPE record) throws IOException, RecordSerializationException {
+//		if(record.getKey().compareTo(new StringField("california"))==0){
+//			System.out.println("Insertando california: " + this.toString());
+//		}
 		this.root.insertRecord(record);
+		
+//		if(record.getKey().compareTo(new StringField("california"))==0){
+//			System.out.println("Insertando california: " + this.toString());
+//		}
+//		if (found) {
+//			if((this.getRecord((KEYTYPE) new StringField("california"))==null)){
+//				System.out.println("Se perdió: " + this.toString());
+//			}
+//		}else{
+//			found=(this.getRecord((KEYTYPE) new StringField("california"))!=null);
+//		}
+//		if(found){
+//			System.out.println("Found: " + this.toString());	
+//		}
+		
 		// TODO implementar balanceo y split para el caso de que quede en
 		// overflow luego de insertar.
 
 		if (this.root.isInOverflow()) {
 			if (this.root.getLevel() == 0) {
-				TreeLeafNode oldRoot = (TreeLeafNode) this.root; 
+				TreeLeafNode oldRoot = (TreeLeafNode) this.root;
 				this.root = this.instantiateRootNode(oldRoot.getLevel() + 1);
 
 				ArrayList<TreeNode> leafs = new ArrayList<TreeNode>();
@@ -153,9 +173,9 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 				leafs.get(0).passMinimumCapacityExcedentToTheRight(leafs.get(1));
 				leafs.get(1).passMinimumCapacityExcedentToTheRight(leafs.get(2));
 
-				((TreeIndexNode)this.root).indexChild(leafs.get(0));
-				((TreeIndexNode)this.root).indexChild(leafs.get(1));
-				((TreeIndexNode)this.root).indexChild(leafs.get(2));
+				((TreeIndexNode) this.root).indexChild(leafs.get(0));
+				((TreeIndexNode) this.root).indexChild(leafs.get(1));
+				((TreeIndexNode) this.root).indexChild(leafs.get(2));
 
 				if (leafs.get(0).isInOverflow() || leafs.get(1).isInOverflow() || leafs.get(2).isInOverflow()) {
 					throw new RuntimeException("ERROR: No se pudo hacer el split. Un nodo quedó en overflow. Pruebe agrandando el tamaño de bloques.");
@@ -164,8 +184,9 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 				this.updateNode(leafs.get(0));
 				this.updateNode(leafs.get(1));
 				this.updateNode(leafs.get(2));
+			}else{
+				throw new RuntimeException("Nodo raiz en overflow, no implementado: " + this.root.toString());
 			}
-
 		}
 		this.updateNode(this.root);
 		return 0;
@@ -173,15 +194,15 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 
 	public TreeNode instantiateNewIndexNodeAndSave(int level) throws BlockFileOverflowException, WrongBlockNumberException, RecordSerializationException, IOException {
 		int newNodeNumber = allocateNodeSpace(NON_ROOT_NODE_BLOCKS_QTY);
-		TreeIndexNode node = new TreeIndexNode(this,newNodeNumber,  1);
+		TreeIndexNode node = new TreeIndexNode(this, newNodeNumber, 1);
 		node.setLevel(level);
 		this.updateNode(node);
 		return node;
 	}
-	
+
 	public TreeNode instantiateNewLeafNodeAndSave() throws BlockFileOverflowException, WrongBlockNumberException, RecordSerializationException, IOException {
 		int newNodeNumber = allocateNodeSpace(NON_ROOT_NODE_BLOCKS_QTY);
-		TreeNode node = new TreeLeafNode(this,newNodeNumber);
+		TreeNode node = new TreeLeafNode(this, newNodeNumber);
 		this.updateNode(node);
 		return node;
 	}
@@ -191,9 +212,9 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 		// la raiz siempre tiene numero de nodo 0
 		TreeNode node = null;
 		if (level == 0) {
-			node = new TreeLeafNode(this,0, ROOT_NODE_BLOCKS_QTY);
+			node = new TreeLeafNode(this, 0, ROOT_NODE_BLOCKS_QTY);
 		} else if (level >= 0) {
-			node = new TreeIndexNode(this,0, ROOT_NODE_BLOCKS_QTY);
+			node = new TreeIndexNode(this, 0, ROOT_NODE_BLOCKS_QTY);
 			((TreeIndexNode) node).setLevel(level);
 		} else {
 			throw new IllegalArgumentException("Condicion Level>0, pero vino: " + level);
@@ -210,11 +231,11 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 		this.blockFile.load();
 		if (this.blockFile.getBlockCount() > Tree.ROOT_NODE_BLOCKS_QTY) {
 			// hay mas nodos que una raiz --> la raiz es un indice
-			this.root = new TreeIndexNode(this,0, Tree.ROOT_NODE_BLOCKS_QTY);
+			this.root = new TreeIndexNode(this, 0, Tree.ROOT_NODE_BLOCKS_QTY);
 		} else {
 			if (this.blockFile.getBlockCount() == Tree.ROOT_NODE_BLOCKS_QTY) {
 				// hay solamente un nodo raiz --> la raiz es hoja
-				this.root = new TreeLeafNode(this,0, Tree.ROOT_NODE_BLOCKS_QTY);
+				this.root = new TreeLeafNode(this, 0, Tree.ROOT_NODE_BLOCKS_QTY);
 			} else {
 				throw new RecordSerializationException("Archivo de B# Tree invÃ¡lido.");
 			}
@@ -222,10 +243,6 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 		// la raiz siempre tiene numero de nodo 0
 		deserializeNode(0, this.root);
 	}
-
-//	public void saveNode(TreeNode node) throws BlockFileOverflowException, WrongBlockNumberException, RecordSerializationException, IOException {
-//		saveNode(node, false);
-//	}
 
 	protected void setRoot(TreeNode root) {
 		this.root = root;
@@ -245,7 +262,7 @@ public class Tree<RECTYPE extends Record<KEYTYPE>, KEYTYPE extends Field> implem
 		}
 		List<byte[]> serializationParts = node.serializeInParts(this.getNodeSize());
 		if (serializationParts.size() > node.getBlockQty()) {
-			throw new BlockFileOverflowException("La cantidad de partes es mayor a la esperada.",node.getBlockQty(),serializationParts.size());
+			throw new BlockFileOverflowException("La cantidad de partes es mayor a la esperada.", node.getBlockQty(), serializationParts.size());
 		}
 		for (int i = 0; i < serializationParts.size(); i++) {
 			byte[] part = serializationParts.get(i);

ba892e75f1b755c0303795969b2ada87986e3ac7
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-09 01:26:05 +0000

    Arreglado error en método getNode del arbol.

--- trunk/src/speakit/io/bsharptree/test/TreeFullTest.java
+++ trunk/src/speakit/io/bsharptree/test/TreeFullTest.java
@@ -41,6 +41,7 @@ public class TreeFullTest {
 
 	private static void verifyCorrectRecord(InvertedIndexIndexRecord record, String word, StringField key) {
 		// verifica que el record obtenido sea el correcto
+		Assert.assertNotNull("El arbol no devolvió el registro que se había insertado.", record);
 		Assert.assertEquals(0, record.getKey().compareTo(key));
 		Assert.assertEquals(simulateBlockNumber(word), record.getBlockNumber());
 	}

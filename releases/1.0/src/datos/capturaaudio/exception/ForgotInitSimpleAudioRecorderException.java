d26a0d5049e9205ca55c4d9f95c5aaeb4d496d39
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-09 21:39:55 +0000

    Como el BSharp todavia no funciona correctamente, la integración del Bsharp en el Indice invertido no pasa las pruebas. Mergear cuando el BSharp esté listo.

--- releases/1.0/src/datos/capturaaudio/exception/ForgotInitSimpleAudioRecorderException.java
+++
@@ -1,9 +0,0 @@
-package datos.capturaaudio.exception;
-
-@SuppressWarnings("serial")
-public class ForgotInitSimpleAudioRecorderException extends SimpleAudioRecorderException {
-
-	public ForgotInitSimpleAudioRecorderException() {
-		super("ERROR- Forgot to call SimpleAudioRecorder->init()");
-	}
-}

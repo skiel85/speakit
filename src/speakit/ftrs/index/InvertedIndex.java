ceb550a7db578e50d300160ead30f7b1c31ca7d2
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-10 10:42:59 +0000

    Integrado el indice invertido con el b#. Pruebas funcionando.

--- src/speakit/ftrs/index/InvertedIndex.java
+++ src/speakit/ftrs/index/InvertedIndex.java
@@ -6,29 +6,40 @@ import java.util.ArrayList;
 import speakit.Configuration;
 import speakit.FileManager;
 import speakit.io.File;
-import speakit.io.blockfile.WrongBlockNumberException;
+import speakit.io.bsharptree.Tree;
 import speakit.io.record.RecordFactory;
 import speakit.io.record.RecordSerializationException;
 import speakit.io.record.StringField;
 import speakit.io.recordfile.DirectRecordFile;
 
-//TODO implementar el índice
 public class InvertedIndex implements File, RecordFactory {
 
-	private static final String	DATAFILE	= "InvertedIndexData.dat";
+	private static final String									DATAFILE		= "InvertedIndexData.dat";
+	private static final String									INDEXFILENAME	= "BSharpTree.dat";
 	private DirectRecordFile<InvertedIndexRecord, StringField>	invertedListsFile;
-	// protected ArrayList<InvertedIndexRecord> records;
+	private Tree<InvertedIndexIndexRecord, StringField>			index;
 
 	public InvertedIndex() {
-		// records = new ArrayList<InvertedIndexRecord>();
+
+	}
+	
+	public InvertedIndexRecord getDocumentsFor(String word) throws RecordSerializationException, IOException {
+		StringField key = new StringField(word);
+		return getRecord(key);
 	}
 
-	// public boolean exists(String word) {
-	// return false;
-	// }
-
-	public InvertedIndexRecord getDocumentsFor(String word) throws RecordSerializationException, IOException { 
-		return invertedListsFile.getRecord(new StringField(word));
+	private InvertedIndexRecord getRecord(StringField key) throws IOException, RecordSerializationException {
+		// utilizo el indice para obtener la posicion del registro en el archivo
+		InvertedIndexIndexRecord indexRecord = index.getRecord(key);
+		if (indexRecord == null) {
+			// la clave no está en el indice
+			return null;
+		} else {
+			int recordBlockNumber = indexRecord.getBlockNumber();
+			// busco el registro en el archivo de datos pero le paso el numero
+			// de bloque
+			return invertedListsFile.getRecord(key, recordBlockNumber);
+		}
 	}
 
 	public void updateRecords(ArrayList<InvertedIndexRecord> records) throws RecordSerializationException, IOException {
@@ -39,26 +50,26 @@ public class InvertedIndex implements File, RecordFactory {
 
 	public void updateRecord(InvertedIndexRecord record) throws RecordSerializationException, IOException {
 		StringField key = record.getKey();
-		InvertedIndexRecord retrievedRecord = this.invertedListsFile.getRecord(key);
-		if (retrievedRecord == null) {
-			int recordBlockNumber = (int) this.invertedListsFile.insertRecord(record);
-			this.indexRecord(record, recordBlockNumber);
+		//registro de indice
+		InvertedIndexIndexRecord indexRecord = index.getRecord(key);		
+		if (indexRecord == null) {
+			//Indexa el registro asociándolo al numero de bloque donde fué insertado
+			long insertedRecordBlock = this.invertedListsFile.insertRecord(record);
+			this.index.insertRecord(new InvertedIndexIndexRecord( key.getString(), (int) insertedRecordBlock));
 		} else {
-			InvertedIndexRecord mergedRecord = mergeRecords(record, retrievedRecord);
-			this.invertedListsFile.updateRecord(mergedRecord);
+			//mergeo los registros
+			InvertedIndexRecord originalRecord = this.invertedListsFile.getRecord(key);
+			InvertedIndexRecord mergedRecord = mergeRecords(record, originalRecord);
+			//actualizo el archivo de datos
+			long updatedBlockNumber = this.invertedListsFile.updateRecord(mergedRecord,indexRecord.getBlockNumber());
+			//si el registro cambió de bloque lo reindexo
+			if((int)updatedBlockNumber!=indexRecord.getBlockNumber()){
+				indexRecord.setBlockNumber((int) updatedBlockNumber);
+				this.index.updateRecord(indexRecord);
+			}
 		}
 	}
-
-	/**
-	 * Indexa el registro asociándolo al numero de bloque donde fué insertado
-	 * 
-	 * @param record
-	 * @param recordBlockNumber
-	 */
-	private void indexRecord(InvertedIndexRecord record, int recordBlockNumber) {
-		// TODO Implementar la indexación
-	}
-
+	
 	/**
 	 * copia todos los elementos de la lista invertida de from a to.
 	 * 
@@ -76,33 +87,36 @@ public class InvertedIndex implements File, RecordFactory {
 		to.setInvertedList(listTo);
 		return to;
 	}
-	
+
 	private void createDataFile(FileManager fileManager) throws IOException {
 		this.invertedListsFile = new DirectRecordFile<InvertedIndexRecord, StringField>(fileManager.openFile(DATAFILE), this);
+		this.index = new Tree<InvertedIndexIndexRecord, StringField>(fileManager.openFile(INDEXFILENAME), InvertedIndexIndexRecord.createRecordFactory());
 	}
-	
+
 	public void load(FileManager filemanager, Configuration conf) throws IOException {
 		createDataFile(filemanager);
 		this.invertedListsFile.load();
+		this.index.load();
 	}
 
 	@Override
 	public void install(FileManager filemanager, Configuration conf) throws IOException {
 		createDataFile(filemanager);
 		this.invertedListsFile.create(conf.getBlockSize());
+		this.index.create(conf.getBlockSize());
 	}
 
 	@Override
 	public boolean isInstalled(FileManager filemanager) throws IOException {
-		return filemanager.exists(DATAFILE);
+		return filemanager.exists(DATAFILE) && filemanager.exists(INDEXFILENAME);
 	}
 
 	@Override
 	public InvertedIndexRecord createRecord() {
 		return new InvertedIndexRecord();
 	}
-	
+
 	public String toString() {
-		return  invertedListsFile.toString();
+		return invertedListsFile.toString();
 	}
 }

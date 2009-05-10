c8fcf5c6788c6c115ab22114912105096e98ed12
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-10 22:31:06 +0000

    no compilaba por falta de un throws

--- trunk/src/speakit/ftrs/test/FTRSImpleTest.java
+++ trunk/src/speakit/ftrs/test/FTRSImpleTest.java
@@ -1,5 +1,7 @@
 package speakit.ftrs.test;
 
+import java.io.IOException;
+
 import junit.framework.Assert;
 
 import org.junit.Before;
@@ -16,7 +18,7 @@ public class FTRSImpleTest {
 		this.ftrs = new FTRSImpl();
 	}
 	@Test
-	public void testApplyFilters(){
+	public void testApplyFilters() throws IOException{
 		TextDocument document = new TextDocument ("habia una casa en donde        estabamos todos, eramos -muchos- y no sabiamos como hacer para resolver el problema.");
 		Assert.assertEquals("habia casa estabamos sabiamos resolver problema", this.ftrs.applyFilters(document).getText());
 	}

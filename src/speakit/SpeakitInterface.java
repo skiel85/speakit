d589a5f37459820a2e22b338ecfed2667c163710
leonardo.giorcelli <leonardo.giorcelli@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-10 16:57:15 +0000

    Correccion del Issue 41, palabras repetidas en el Menu

--- src/speakit/SpeakitInterface.java
+++ src/speakit/SpeakitInterface.java
@@ -24,7 +24,7 @@ public interface SpeakitInterface extends Installable {
 	 * Agrega los documentos al sistema y devuelve la lista de documentos
 	 * para luego obtener las palabras desconocidas de cada uno
 	 */
-	public abstract Iterable<TextDocument> addDocuments(TextDocumentList docs) throws IOException;
+	public abstract Iterable<String> addDocuments(TextDocumentList docs) throws IOException;
 	
 	/**
 	 * Devuelve una documento de audio

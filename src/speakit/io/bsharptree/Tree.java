d95eafac7e8fd3ed5da468ea58b32b1d9ab85353
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-10 08:25:03 +0000

    Bug fixed: indexaba mal la clave mas chica. Pasa la prueba TreeFullTest2!

--- src/speakit/io/bsharptree/Tree.java
+++ src/speakit/io/bsharptree/Tree.java
@@ -14,6 +14,7 @@ import speakit.io.record.Field;
 import speakit.io.record.Record;
 import speakit.io.record.RecordFactory;
 import speakit.io.record.RecordSerializationException;
+import speakit.io.record.StringField;
 import speakit.io.recordfile.RecordFile;
 
 @SuppressWarnings("unchecked")

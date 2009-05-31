3ba5dce15ac7cd9f629d1eff7d7b596e2a12c049
nahuelquevedo <nahuelquevedo@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-05-31 16:59:12 +0000

    Aritmétio: Underflow implementado

--- trunk/src/speakit/compression/arithmetic/test/RangeTest.java
+++ trunk/src/speakit/compression/arithmetic/test/RangeTest.java
@@ -93,20 +93,44 @@ public class RangeTest {
 	@Test
 	public void testFlushClearsBuffer() {
 		String overflowRoof = "0011" ;
-		sut.setRoof(overflowRoof);//aca deberia simplificarse el overflow
-		sut.simplify();
+		sut.setRoof(overflowRoof);
+		sut.simplify();//aca deberia resolverse el overflow
 		Assert.assertEquals(initialRoof, sut.getRoof());//deberia quedar igual que antes
 		Assert.assertEquals("00", sut.flush());
 		Assert.assertEquals("", sut.flush());	
 	}
 	
-	@Ignore
 	@Test
-	public void testUnderflow() { 
+	public void testUnderflow(){
+		sut.setRoof( "1010");
+		sut.setFloor("0101");
+		sut.simplify();
+		Assert.assertEquals("1101", sut.getRoof());
+		Assert.assertEquals("0010", sut.getFloor());
+		Assert.assertEquals("", sut.flush());
+	}
+	
+	@Test
+	public void testUnderflowEmision(){
+		sut.setRoof( "1010");
+		sut.setFloor("0101");
+		sut.simplify();//esto provoca un underflow de 1
+		sut.setRoof( "0011");
+		sut.setFloor("0000");
+		sut.simplify();//esto provoca un overflow de 00 		
+		Assert.assertEquals("010", sut.flush());//emite el overflow y el underflow
+		sut.setRoof( "0011");
+		sut.setFloor("0000");
+		sut.simplify();//esto provoca un overflow de 00 sin emision de underflow
+		Assert.assertEquals("00", sut.flush());
 	}
 	
 	@Ignore
 	@Test
 	public void testWithRemainingUnderflowClose() { 
-	} 
+	}
+	
+	//simplifyTwice
+	//varios overflows
+	//mezclar overflow y underflow
 }

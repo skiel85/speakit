6e0f527841ceb732325edb9f45b2c1db242df204
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-06-01 19:51:36 +0000

    Testeado getProbability de la tabla de probabilidades. Agregado método hashCode a Symbol.

--- src/speakit/compression/arithmetic/test/ProbabilityTableTest.java
+++ src/speakit/compression/arithmetic/test/ProbabilityTableTest.java
@@ -3,32 +3,32 @@ package speakit.compression.arithmetic.test;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import speakit.compression.arithmetic.ProbabilityTable;
-import speakit.compression.arithmetic.Range;
 import speakit.compression.arithmetic.Symbol;
-public class ProbabilityTableTest {
-	
-	private ProbabilityTable sut;
 
+public class ProbabilityTableTest {
+
+	private ProbabilityTable sut;
 
 	@Before
 	public void setUp() throws Exception {
 		sut = new ProbabilityTable();
-		//sut.add(new Symbol('a'), 3);
-		//sut.add(new Symbol('b'), 5);
-		
+		sut.add(Symbol.getEscape(), 8);
+		sut.add(new Symbol('a'), 3);
+		sut.add(new Symbol('b'), 5);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 
 	}
-	
+
 	@Test
 	public void testGetProbability() {
-		
+		Assert.assertEquals(0.5, sut.getProbability(Symbol.getEscape()), 0);
+		Assert.assertEquals(0.1875, sut.getProbability(new Symbol('a')), 0);
+		Assert.assertEquals(0.3125, sut.getProbability(new Symbol('b')), 0);
 	}
 }

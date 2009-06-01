6b74689662aa63907883f981818dad9d378eb761
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-06-01 20:40:22 +0000

    Creado un test con más símbolos para la tabla de probabilidades.

---
+++ src/speakit/compression/arithmetic/test/ProbabilityTableBasicTest.java
@@ -0,0 +1,41 @@
+package speakit.compression.arithmetic.test;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import speakit.compression.arithmetic.ProbabilityTable;
+import speakit.compression.arithmetic.Symbol;
+
+public class ProbabilityTableBasicTest {
+
+	private ProbabilityTable sut;
+
+	@Before
+	public void setUp() throws Exception {
+		sut = new ProbabilityTable();
+		sut.add(Symbol.getEscape(), 8);
+		sut.add(new Symbol('a'), 3);
+		sut.add(new Symbol('b'), 5);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	public void testGetProbability() {
+		Assert.assertEquals(0.5000, sut.getProbability(Symbol.getEscape()), 0);
+		Assert.assertEquals(0.1875, sut.getProbability(new Symbol('a')), 0);
+		Assert.assertEquals(0.3125, sut.getProbability(new Symbol('b')), 0);
+	}
+
+	@Test
+	public void testGetDistribution() {
+		Assert.assertEquals(0.5000, sut.getDistribution(Symbol.getEscape()), 0);
+		Assert.assertEquals(0.6875, sut.getDistribution(new Symbol('a')), 0);
+		Assert.assertEquals(1.0000, sut.getDistribution(new Symbol('b')), 0);
+	}
+}
--- src/speakit/compression/arithmetic/test/ProbabilityTableTest.java
+++ src/speakit/compression/arithmetic/test/ProbabilityTableTest.java
@@ -14,10 +14,16 @@ public class ProbabilityTableTest {
 
 	@Before
 	public void setUp() throws Exception {
+		// JERRYRICE
 		sut = new ProbabilityTable();
-		sut.add(Symbol.getEscape(), 8);
-		sut.add(new Symbol('a'), 3);
-		sut.add(new Symbol('b'), 5);
+		sut.add(Symbol.getEscape(), 7);
+		sut.add(new Symbol('C'), 1);
+		sut.add(new Symbol('E'), 2);
+		sut.add(new Symbol('I'), 1);
+		sut.add(new Symbol('J'), 1);
+		sut.add(new Symbol('R'), 3);
+		sut.add(new Symbol('Y'), 1);
+
 	}
 
 	@After
@@ -27,15 +33,24 @@ public class ProbabilityTableTest {
 
 	@Test
 	public void testGetProbability() {
-		Assert.assertEquals(0.5000, sut.getProbability(Symbol.getEscape()), 0);
-		Assert.assertEquals(0.1875, sut.getProbability(new Symbol('a')), 0);
-		Assert.assertEquals(0.3125, sut.getProbability(new Symbol('b')), 0);
+		Assert.assertEquals(0.4375, sut.getProbability(Symbol.getEscape()), 0);
+		Assert.assertEquals(0.0625, sut.getProbability(new Symbol('C')), 0);
+		Assert.assertEquals(0.1250, sut.getProbability(new Symbol('E')), 0);
+		Assert.assertEquals(0.0625, sut.getProbability(new Symbol('I')), 0);
+		Assert.assertEquals(0.0625, sut.getProbability(new Symbol('J')), 0);
+		Assert.assertEquals(0.1875, sut.getProbability(new Symbol('R')), 0);
+		Assert.assertEquals(0.0625, sut.getProbability(new Symbol('Y')), 0);
+
 	}
 
 	@Test
-	public void testGetProbabilityUntil() {
-		Assert.assertEquals(0.5000, sut.getDistribution(Symbol.getEscape()), 0);
-		Assert.assertEquals(0.6875, sut.getDistribution(new Symbol('a')), 0);
-		Assert.assertEquals(1.0000, sut.getDistribution(new Symbol('b')), 0);
+	public void testGetDistribution() {
+		Assert.assertEquals(0.4375, sut.getDistribution(Symbol.getEscape()), 0);
+		Assert.assertEquals(0.5000, sut.getDistribution(new Symbol('C')), 0);
+		Assert.assertEquals(0.6250, sut.getDistribution(new Symbol('E')), 0);
+		Assert.assertEquals(0.6875, sut.getDistribution(new Symbol('I')), 0);
+		Assert.assertEquals(0.7500, sut.getDistribution(new Symbol('J')), 0);
+		Assert.assertEquals(0.9375, sut.getDistribution(new Symbol('R')), 0);
+		Assert.assertEquals(1.0000, sut.getDistribution(new Symbol('Y')), 0);
 	}
 }

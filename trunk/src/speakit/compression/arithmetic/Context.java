5800f16c7a6392563b64da3c8f6089fe166b0961
leonardo.giorcelli <leonardo.giorcelli@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-06-04 23:38:12 +0000

    Avances en el LZP, sin funcionar todavia

--- trunk/src/speakit/compression/arithmetic/Context.java
+++ trunk/src/speakit/compression/arithmetic/Context.java
@@ -1,8 +1,11 @@
 package speakit.compression.arithmetic;
 
+import java.util.ArrayList;
+
 
 public class Context {
 
+		private ArrayList<Symbol> chars;
 		
 		/**
 		 */
@@ -14,6 +17,7 @@ public class Context {
 		/**
 		 */
 		public Context(int maxDepth){
+			chars = new ArrayList<Symbol>(maxDepth);
 		}
 				
 		/**
@@ -21,4 +25,27 @@ public class Context {
 		public void add(Symbol symbol){
 		}
 
+		public Integer size() {
+			return chars.size();
+		}
+		
+		public String toString() {
+			String out = "";
+			for (Symbol symbol : chars) {
+				out.concat(symbol.toString());
+			}
+			return out;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Context) {
+				Context other = (Context) obj;
+				if (this.size() == other.size() && this.toString().equals(other.toString()) ) {
+					return true;
+				}
+			}	
+			return false;
+		}
+		
 }

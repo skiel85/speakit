61555e493752fa2f5380a1a03f880f86b5cfb970
skiel85 <skiel85@793bc9a0-1433-11de-babb-ddc7aea8d8a1>
2009-06-16 01:43:21 +0000

    Eliminado el código, se subió código erróneo.

--- trunk/releases/3.0/src/speakit/io/bsharptree/TreeNodeElement.java
+++
@@ -1,9 +0,0 @@
-package speakit.io.bsharptree;
-
-import speakit.io.record.CompositeField;
-import speakit.io.record.Field;
-
-public abstract class TreeNodeElement extends CompositeField {
-	public abstract Field getKey();
-
-}
--- trunk/releases/3.0/src/speakit/io/record/FieldFactory.java
+++
@@ -1,7 +0,0 @@
-package speakit.io.record;
-
-public interface FieldFactory {
-
-	public Field createField();
-
-}
